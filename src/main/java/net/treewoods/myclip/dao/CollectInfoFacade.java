6ff6ce5ca3d9c72cfc765c73bf014a31ff2d9b3f
木戸　亨 <kido.toru.wood@gmail.com>
2014-03-27 23:49:12 +0900

    no message

--- src/main/java/net/treewoods/myclip/dao/CollectInfoFacade.java
+++ src/main/java/net/treewoods/myclip/dao/CollectInfoFacade.java
@@ -28,5 +28,9 @@ public class CollectInfoFacade extends AbstractFacade<CollectInfo> {
 	public CollectInfoFacade() {
 		super(CollectInfo.class);
 	}
+
+	public int findMaxCollectId() {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
 	
 }
---
+++ src/main/java/net/treewoods/myclip/dao/VArticleFacade.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package net.treewoods.myclip.dao;
+
+import java.util.List;
+import net.treewoods.myclip.entity.VArticle;
+
+/**
+ *
+ * @author toru
+ */
+public class VArticleFacade {
+
+	public List<VArticle> findByCollectId(int currentId) {
+		throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	}
+	
+}
